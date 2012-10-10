1e9f3f3c929e988cdd39c6e0af52a37d5b52b29b
Daniel Gaubert <danielgarciaaubert@gmail.com>
2012-10-10 18:14:29 +0200

    While implementig feature #1, improved layout skeleton

--- public/javascript/mammoth.js
+++ public/javascript/mammoth.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  
+  $('input.date').datepicker();
+  
   $('ul.pagination li.pagination-icon').hover(
     function() { $(this).addClass('ui-state-hover'); }, 
 	function() { $(this).removeClass('ui-state-hover'); }
