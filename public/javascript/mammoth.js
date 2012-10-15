a7ac076d3d21a456a188dd045c1dee12a175b3f1
dgaubert <danielgarciaaubert@gmail.com>
2012-10-15 23:12:34 +0200

    Implementing feature #1, improved skeleton layout

--- public/javascript/mammoth.js
+++ public/javascript/mammoth.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
   
-  $('input.date').datepicker();
+  $('input.date').datepicker({dateFormat: 'dd/mm/yy'});
+
+  $('a#search').button({icons: {primary: "ui-icon-search"}});
+  $('h3.tag1').button().addClass('link');;
   
   $('ul.pagination li.pagination-icon').hover(
     function() { $(this).addClass('ui-state-hover'); }, 
-	function() { $(this).removeClass('ui-state-hover'); }
+    function() { $(this).removeClass('ui-state-hover'); }
   );
+
 });
