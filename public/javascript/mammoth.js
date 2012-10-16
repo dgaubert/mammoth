80d545080bf2a0283f0aaa9baacc899c22901e47
Daniel Gaubert <danielgarciaaubert@gmail.com>
2012-10-16 17:57:59 +0200

    Implementing feature #1, improved skeleton layout

--- public/javascript/mammoth.js
+++ public/javascript/mammoth.js
@@ -1,13 +1,51 @@
 $(document).ready(function() {
   
+  $('input').addClass('ui-corner-all');
+  
   $('input.date').datepicker({dateFormat: 'dd/mm/yy'});
 
-  $('a#search').button({icons: {primary: "ui-icon-search"}});
-  $('h3.tag1').button().addClass('link');;
+  $('a#search').button({
+    icons: {
+      primary: "ui-icon-search"
+    }
+  });
   
-  $('ul.pagination li.pagination-icon').hover(
-    function() { $(this).addClass('ui-state-hover'); }, 
-    function() { $(this).removeClass('ui-state-hover'); }
-  );
+  $('a.link').button();
+  
+  $('a.tag').button();
+  
+  $('a#previous').button({
+    text: false,
+    icons: {
+      primary: "ui-icon-carat-1-w"
+    }
+  });
+  
+  $('a#page-selector')
+    .button()
+    .click(function() {
+      var menu = $('ul#pages').show().position({
+          my: 'left top',
+          at: 'left bottom',
+          of: this
+      });
+      $(document).one('click', function() {
+          menu.hide();
+      });
+      return false;
+  });
+  
+  $('a#next').button({
+    text: false,
+    icons: {
+      primary: "ui-icon-carat-1-e"
+    }
+  });
+  
+  $('div.paginator').buttonset();
+  
+  $('a#next').addClass('ui-corner-right');
+  
+  $('ul#pages').hide().menu();
 
 });
--- routes/blog.js
+++ routes/blog.js
@@ -3,27 +3,36 @@ exports.view = function(req, res){
   // Fake posts database
 
   var posts = [
-      {  title: 'TinyOS 2.x',
-         author: 'Daniel Gaubert',
-         created: '10/10/2010',
-		 slug: 'tinyos-2',
-         abstract: 'Macros are a powerful feature of LispyScript. They are much more powerful than C  define macros. While C #define macros do string substitution, LispyScript macros are code generators Functions take values as arguments and return a value. Macros take code as arguments, and then return ode. Understanding this difference and its ramifications is the key to writing proper macros. Functions get evaluated at runtime. Macros get evaluated at compile time, or pre-compile time to be more precise.',
-         tags: ['wsn','tinyos','sensor']
+      {  
+        title: 'TinyOS 2.x',
+        author: 'Daniel Gaubert',
+        created: '10/10/2010',
+        slug: 'tinyos-2',
+        abstract: 'Macros are a powerful feature of LispyScript. They are much more powerful than C  define macros. While C #define macros do string substitution, LispyScript macros are code generators Functions take values as arguments and return a value. Macros take code as arguments, and then return ode. Understanding this difference and its ramifications is the key to writing proper macros. Functions get evaluated at runtime. Macros get evaluated at compile time, or pre-compile time to be more precise.',
+        tags: ['wsn','tinyos','sensor']
       }
-    , {  title: 'Git model branching', 
-         author: 'Daniel Gaubert',
-         created: '09/12/2010',
-		 slug: 'git-model-branching',
-         abstract: 'Macros are a powerful feature of LispyScript. They are much more powerful than C  define macros. While C define macros do string substitution, LispyScript macros are code generators take values as arguments and a value. Macros take code as arguments, and then code. Understanding  difference and its ramifications is the key to writing proper macros.Functions get evaluated at runtime. Macros get evaluated at compile time, or pre-compile time to be more precise.',
-         tags: ['workflow','git']
+    , {
+        title: 'Git model branching', 
+        author: 'Daniel Gaubert',
+        created: '09/12/2010',
+        slug: 'git-model-branching',
+        abstract: 'Macros are a powerful feature of LispyScript. They are much more powerful than C  define macros. While C define macros do string substitution, LispyScript macros are code generators take values as arguments and a value. Macros take code as arguments, and then code. Understanding  difference and its ramifications is the key to writing proper macros.Functions get evaluated at runtime. Macros get evaluated at compile time, or pre-compile time to be more precise.',
+        tags: ['workflow','git']
       }
-    , {  title: 'LispyScript Macros',
-         author: 'Daniel Gaubert',
-         created: '21/11/2010',
-		 slug: 'lispyscript-macros',
-         abstract: 'We cannot use a function to reuse this code pattern, because the parts that change are parts of the code. Functions are about reusing code patterns, where it is only the data that changes. Macros are about reusing code patterns, where the code can also change. In LispyScript, we can write a macro to reuse this code pattern. The macro needs a name, let’s call it template as it happens to be a template compiler',
-         tags: ['macros','script']
+    , {
+        title: 'LispyScript Macros',
+        author: 'Daniel Gaubert',
+        created: '21/11/2010',
+        slug: 'lispyscript-macros',
+        abstract: 'We cannot use a function to reuse this code pattern, because the parts that change are parts of the code. Functions are about reusing code patterns, where it is only the data that changes. Macros are about reusing code patterns, where the code can also change. In LispyScript, we can write a macro to reuse this code pattern. The macro needs a name, let’s call it template as it happens to be a template compiler',
+        tags: ['macros','script']
       }
   ];
-  res.render('blog', { title: 'Blog de Daniel García Aubert', posts: posts, pagination: '1-10 de 34' });
+  
+  // Fake pagination
+  var pagination = {
+    selected: '1-10 de 34',
+    pages: ['11-20','21-30','31-34']
+  };
+  res.render('blog', { title: 'Blog de Daniel García Aubert', posts: posts, pagination: pagination });
 };
