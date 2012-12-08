d21cab25c4e5d5a5bb535c8368d81807b1b09094
dgaubert <danielgarciaaubert@gmail.com>
2012-12-08 01:36:00 +0100

    Implementing feature #1. Added post layout, final commit

--- routes/blog.js
+++ routes/blog.js
@@ -80,16 +80,55 @@ exports.categories = function(req, res){
 
 
 exports.view = function(req, res){
+  console.log("Slug:" + req.params.id);
   // Fake post database
   var post = {  
+    title: 'TinyOS 2.x',
+    author: 'Daniel Gaubert',
+    created: '10/10/2010',
+    slug: 'tinyos-2',
+    category: 'development',
+    content: 'Macros are a powerful feature of LispyScript. They are much more powerful than C  define macros. While C #define macros do string substitution, LispyScript macros are code generators Functions take values as arguments and return a value. Macros take code as arguments, and then return ode. Understanding this difference and its ramifications is the key to writing proper macros. Functions get evaluated at runtime. Macros get evaluated at compile time, or pre-compile time to be more precise.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?',
+    tags: ['wsn','tinyos','sensor'],
+    comments: '6'
+  };
+  var categories = [
+    {
+      name: 'desarrollo',
+      count: '25'
+    },
+    {
+      name: 'miscelanea',
+      count: '14'
+    },
+    {
+      name: 'marketing',
+      count: '2'
+    },
+    {
+      name: 'diseño',
+      count: '45'
+    },
+    {
+      name: 'otros',
+      count: '2'
+    }
+  ];
+  
+  var similars = [
+      {  
         title: 'TinyOS 2.x',
-        author: 'Daniel Gaubert',
-        created: '10/10/2010',
         slug: 'tinyos-2',
-        category: 'development',
-        content: 'Macros are a powerful feature of LispyScript. They are much more powerful than C  define macros. While C #define macros do string substitution, LispyScript macros are code generators Functions take values as arguments and return a value. Macros take code as arguments, and then return ode. Understanding this difference and its ramifications is the key to writing proper macros. Functions get evaluated at runtime. Macros get evaluated at compile time, or pre-compile time to be more precise.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?',
-        tags: ['wsn','tinyos','sensor'],
-        comments: '6'
-      };
-  res.render('post', { title: post.title, section: 'blog', post: post });
+      }
+    , {
+        title: 'Git model branching', 
+        slug: 'git-model-branching',
+      }
+    , {
+        title: 'LispyScript Macros',
+        slug: 'lispyscript-macros',
+      }
+  ];
+
+  res.render('post', { title:post.title, section:'blog', post:post, categories:categories, similars:similars});
 };
