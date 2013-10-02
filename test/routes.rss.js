423cb00a17bb63bb9b9078a2bbe0e68036211493
dgaubert <danielgarciaaubert@gmail.com>
2013-10-02 18:02:08 +0200

    Feature #46, refactored controller in order to test it

--- test/routes.rss.js
+++ test/routes.rss.js
@@ -1,30 +1,35 @@
-var rss = require('../lib/routes/rss');
+var express = require('express'),
+    rss = require('../lib/routes/rss'),
+    Article = require('../models/article'),
+    sinon = require('sinon');
     
 describe('routes/rss', function () {
-  var articles = [];
+  var articleStub,
+      appMock;
   
   describe('.createFeed(articles)', function () {
   
     before(function () {
-      var article = {},
-          i = 0;
-
-      for (i = 0; i < 5; i++) {
-        article.title = 'Title ' + i;
-        article.slug = '/slug/' + i;
-        article.abstract = 'Abstract ' + i;
-        article.date = new Date();
-        
-        articles.push(article);
-      }
+      articleStub = sinon.stub(Article.prototype.base.Model, 'find', function () {
+        var article = {},
+            i;
+        for (i = 0; i < 5; i++) {
+          article.title = 'Title ' + i;
+          article.slug = '/slug/' + i;
+          article.abstract = 'Abstract ' + i;
+          article.date = new Date();
+          
+          articles.push(article);
+        }
+        return articles;
+      });
+      
+      app = sinon.stub(
+      
     });
   
     it('Feed should be created', function () {
-      var feed = rss.createFeed(articles);
       
-      feed.should.be.an.instanceof(Object);
-      feed.items.should.be.an.instanceof(Array);
-      feed.items.should.have.length(5);
     });
   });
 
