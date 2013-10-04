f7b358f70e6ee98d6f4aa574774997dc6c0089e3
dgaubert <danielgarciaaubert@gmail.com>
2013-10-04 14:26:13 +0200

    Feature #46, fixed rss tests

--- test/routes.rss.js
+++ test/routes.rss.js
@@ -2,71 +2,61 @@ var sinon = require('sinon');
     
 describe('routes/rss', function () {
 
-  var rss = require('../lib/routes/rss');
+  var articles = [{
+        title: 'Title',
+        slug: '/slug',
+        abstract: 'Abstract',
+        created: new Date()
+      }],
+      Article = {
+        findAll: function(sort, callback) {
+          callback(null, articles);
+        }
+      },
+      rss = require('../lib/routes/rss'),
+      req = {},
+      res = {
+        set: function () {},
+        send: function () {}
+      },
+      next = function () {};
+
+  before(function () {
+    rss.init(Article);
+  });
 
   describe('.getFeed(req, res, next)', function () {
-    
-    it('Response sended', function () {
-      
-      var ArticleMock = {
-            findAll: function(sort, cb) {
-              var article = {},
-                  articles = [];
-              for (var i = 0; i < 5; i++) {
-                article.title = 'Title ' + i;
-                article.slug = '/slug/' + i;
-                article.abstract = 'Abstract ' + i;
-                article.created = new Date();
-                
-                articles.push(article);
-              }
 
-              cb(null, articles);
-            }
-          },
-          req = {},
-          res = {
-            set: function (prop, val) {}, 
-            send: function (view) {}
-          },
-          next = function () {};
+    it('Articles should be gotten', function () {
+      var ArticleMock = sinon.mock(Article);
+      ArticleMock.expects('findAll').once();
 
-      rss.init(ArticleMock);
       rss.getFeed(req, res, next);
 
+      ArticleMock.verify();
+    });
+    
+    it('Response should be sended', function () {
+      var resMock = sinon.mock(res);
+      resMock.expects('set').once().withArgs('Content-Type', 'application/rss+xml');
+      resMock.expects('send').once();
+
+      rss.getFeed(req, res, next);
+
+      resMock.verify();
     });
 
+    describe('.createFeed(articles)', function () {
 
-  // describe.skip('.createFeed(articles)', function () {
+      it('Feed should be created', function () {
+        var feed = rss.createFeed(articles);
+       
+        feed.should.be.an.instanceof(Object);
+        feed.items.should.be.an.instanceof(Array);
+        feed.items.should.have.length(1);
+      });
 
-  //   var articles = [];
-
-  //   before(function () {
-  //     var article = {};
-
-  //     for (var i = 0; i < 5; i++) {
-  //       article.title = 'Title ' + i;
-  //       article.slug = '/slug/' + i;
-  //       article.abstract = 'Abstract ' + i;
-  //       article.date = new Date();
-        
-  //       articles.push(article);
-  //     }
-
-
-  //   });
-
-  //   it('Feed should be created', function () {
-  //     rss.init()
-  //     var feed = rss.createFeed(articles);
-      
-  //     feed.should.be.an.instanceof(Object);
-  //     feed.items.should.be.an.instanceof(Array);
-  //     feed.items.should.have.length(5);
-  //   });
-
-  // });
-  
+    });
 
   });
 
