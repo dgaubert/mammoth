7193b5bc3b1fc42cfb8969d1961982b093ff7c04
dgaubert <danielgarciaaubert@gmail.com>
2013-11-20 17:55:24 +0100

    Feature #59, implementing ajax post -> red test

--- public/js/mammoth.js
+++ public/js/mammoth.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
-  var checkComment = function() {
-    var comment = $('form#comment').serializeArray(),
-        tags = comment[2].value.match(/<(.*?)>/g),
+
+  var checkComment = function(comment) {
+    var tags = comment[2].value.match(/<(.*?)>/g),
         closed,
         tag,
         i,
         j;
+
     if (comment[0].value.match(/^[\w\s._-]+$/) === null) {
-      $('form#comment').find('#error-msg').text('Nombre no válido');
+      $('form#comment')
+        .find('#error-msg')
+        .text('Nombre no válido');
       return 0;
     }
     if (comment[1].value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/) === null) {
-      $('form#comment').find('#error-msg').text('Correo no válido');
+      $('form#comment')
+        .find('#error-msg')
+        .text('Correo no válido');
       return 0;
     }
     if (!comment[2].value.length) {
       $('form#comment').find('#error-msg').text('Escribe un comentario');
       return 0;
     }
-
     /*else {
       for (i = 0; i < tags.length; i++) {
         if (tags[i].search('<a href=') < 0 && tags[i].search('</a>') < 0 &&
@@ -48,41 +52,37 @@ $(document).ready(function() {
           }
         }
       }
-    }*/
+    }
+    */
+    if(comment[4].value === undefined || comment[4].value === null) {
+      $('form#comment')
+        .find('#error-msg')
+        .text('Elije una opción para demostar que es humano');
+      return 0;
+    }
     return 1;
   };
 
-  var isHuman = function () {
+  $('button#comment').on('click', function () {
     var comment = $('form#comment').serializeArray();
 
-    if(comment[4].value === undefined || comment[3].value === null) {
-      $('form#comment').find('#error-msg').text('Elije una opción para demostar que es humano');
-      return 0;
+    if (checkComment(comment)) {
+
+      $.post(location.pathname, comment)
+        .done(function() {
+
+          // Redirect
+
+        })
+        .fail(function(error) {
+          $('form#comment')
+            .find('#error-msg')
+            .text(error);
+
+        });
+
     }
 
-    $.post('/blog/human', {
-        id: comment[3].value,
-        option: {
-          value: comment[4].value
-        }
-      })
-      .done(function() {
-        alert('done');
-        return 1;
-      })
-      .fail(function() {
-        alert('fail');
-        $('form#comment').find('#error-msg').text('Ups! Parece que no eres humano.');
-        return 0;
-      });
-  };
-
-  $('button#comment').on('click', function (e) {
-    if (!checkComment()) {
-      e.preventDefault();
-    } else if (!isHuman()) {
-      e.preventDefault();
-    }
   });
 
 });
