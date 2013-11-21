2ced2062705b3dc717ac8ab7a4aeb72a0affbe35
dgaubert <danielgarciaaubert@gmail.com>
2013-11-21 17:51:24 +0100

    Feature #59, implemented feature

--- public/js/mammoth.js
+++ public/js/mammoth.js
@@ -10,26 +10,33 @@ $(document).ready(function() {
     if (comment[0].value.match(/^[\w\s._-]+$/) === null) {
       $('form#comment')
         .find('#comment-msg')
+        .removeClass('text-info')
+        .addClass('text-danger')
         .text('Nombre no válido');
       return 0;
     }
     if (comment[1].value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/) === null) {
       $('form#comment')
         .find('#comment-msg')
+        .removeClass('text-info')
+        .addClass('text-danger')
         .text('Correo no válido');
       return 0;
     }
     if (!comment[2].value.length) {
       $('form#comment').find('#comment-msg').text('Escribe un comentario');
       return 0;
-    }
-    /*else {
+    } else if (tags !== null) {
       for (i = 0; i < tags.length; i++) {
         if (tags[i].search('<a href=') < 0 && tags[i].search('</a>') < 0 &&
           tags[i].search('<code>') < 0 && tags[i].search('</code>') < 0 &&
           tags[i].search('<strong>') < 0 && tags[i].search('</strong') < 0 &&
           tags[i].search('<blockquote>') < 0 && tags[i].search('</blockquote>') < 0) {
-          $('form#comment').find('#comment-msg').text('Etiqueta HTML ' + tags[i] + ' no válida');
+          $('form#comment')
+            .find('#comment-msg')
+            .removeClass('text-info')
+            .addClass('text-danger')
+            .text('Etiqueta HTML ' + tags[i] + ' no válida');
           return 0;
         }
         closed = false;
@@ -47,17 +54,25 @@ $(document).ready(function() {
             }
           }
           if (!closed) {
-            $('form#comment').find('#comment-msg').text('Etiqueta HTML ' + tags[i] + ' no cerrada');
+            $('form#comment')
+              .find('#comment-msg')
+              .removeClass('text-info')
+              .addClass('text-danger')
+              .text('Etiqueta HTML ' + tags[i] + ' no cerrada');
             return 0;
           }
         }
       }
     }
-    */
-    if(comment[4].value === undefined || comment[4].value === null) {
+
+    if(comment[4] === undefined ||
+        comment[4].value === undefined ||
+        comment[4].value === null) {
       $('form#comment')
         .find('#comment-msg')
-        .text('Elije una opción para demostar que es humano');
+        .removeClass('text-info')
+        .addClass('text-danger')
+        .text('Elije una opción para demostar que eres humano');
       return 0;
     }
     return 1;
@@ -68,23 +83,31 @@ $(document).ready(function() {
     var comment = $('form#comment').serializeArray();
 
     if (checkComment(comment)) {
-      $.post(location.pathname, comment)
+      $.post(location.pathname  + '/comment', comment)
         .done(function(msg) {
           $('form#comment')
             .find('#comment-msg')
+            .removeClass('text-danger')
+            .addClass('text-info')
             .text(msg);
           setTimeout(function () {
-            location.reload();
+            location.pathname = location.pathname + '#comments';
+            location.reload(true);
           }, 1000);
         })
-        .fail(function(jqXHR, error, errorThrown) {
+        .fail(function(jqXHR) {
           $('form#comment')
             .find('#comment-msg')
-            .text(errorThrown);
+            .removeClass('text-info')
+            .addClass('text-danger')
+            .text(jqXHR.responseText);
         });
 
     }
 
   });
+
+      /*
+    */
 
 });
