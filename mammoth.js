71f6ebb2a451093ec25c330e43884905d473d2bc
dgaubert <danielgarciaaubert@gmail.com>
2013-12-08 02:48:36 +0100

    Feature #62, refactored GridFS

--- mammoth.js
+++ mammoth.js
@@ -4,9 +4,17 @@
 require('./lib/conf/env');
 
 // For testing purpose it's needed to export the application
-var app = module.exports = require('./lib/app');
+var mongoose = require('mongoose'),
+    app = module.exports = require('./lib/app');
 
-// Server
-app.listen(process.env.PORT, process.env.IP, function () {
-  console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT);
+mongoose.connect(require('./lib/conf/db'), function (err, res) {
+  if (err) {
+    console.log('ERROR: connecting to DB. ' + err);
+  } else {
+    // Server
+    app.listen(process.env.PORT, process.env.IP, function () {
+      console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT);
+    });
+  }
 });
+
