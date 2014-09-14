3c819e975148e9343c6fe6fd7f7eea17f4ee40bb
dgaubert <danielgarciaaubert@gmail.com>
2014-09-15 00:40:11 +0200

    Big refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,33 @@
 module.exports = function (grunt) {
+  var srcFiles = [
+    'mammoth.js',
+    'lib/**/*.js',
+    'test/**/*.js',
+  ];
 
-  module.exports = function (grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-    });
+  var publicFiles = [
+    'public/js/**/*.js'
+  ];
 
-    // load plugins
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-shell');
+  var monitoredFiles = [
+    'gruntfile.js',
+    'mammoth.js',
+    'lib/**/*.js',
+    'test/**/*.js',
+    'public/js/**/*.js'
+  ];
 
-    grunt.registerTask('ut', ['watch:ut']);
-
-  };
+  var publicFiles = [
+    'public/vendor/jquery/jquery-1.8.2.min.js',
+    'public/vendor/bootstrap/js/bootstrap.min.js',
+    'public/js/*.js',
+    'public/less/*.less'
+  ];
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+      files: monitoredFiles,
       options: {
         expr: true,
         globals: {
@@ -27,28 +38,19 @@ module.exports = function (grunt) {
       }
     },
     mochaTest: {
+      options: {
+        reporter: 'dot',
+        require: 'should',
+        ui: 'bdd',
+        clearRequireCache: true
+      },
       all: {
-        options: {
-          reporter: 'dot',
-          require: 'should',
-          ui: 'bdd'
-        },
         src: ['test/**/*.js']
       },
       unit: {
-        options: {
-          reporter: 'dot',
-          require: 'should',
-          ui: 'bdd'
-        },
         src: ['test/unit/**/*.js']
       },
       integration: {
-        options: {
-          reporter: 'dot',
-          require: 'should',
-          ui: 'bdd'
-        },
         src: ['test/integration/**/*.js']
       }
     },
@@ -83,65 +85,35 @@ module.exports = function (grunt) {
         }
       }
     },
-    nodemon: {
-      dev: {
-        script: '<%= pkg.name %>.js',
+    watch: {
+      ut: {
+        files: monitoredFiles,
+        tasks: ['mochaTest:unit'],
         options: {
-          nodeArgs: ['--debug']
+            livereload: true
         }
       },
-      options: {
-        callback: function (nodemon) {
-          nodemon.on('log', function (event) {
-            console.log(event.colour);
-          });
-
-          // opens browser on initial server start
-          nodemon.on('config:update', function () {
-            // Delay before server listens on port
-            setTimeout(function() {
-              require('open')('http://localhost:8000');
-            }, 1000);
-          });
-
-          // refreshes browser when server reboots
-          nodemon.on('restart', function () {
-            // Delay before server listens on port
-            setTimeout(function() {
-              require('fs').writeFileSync('.rebooted', 'rebooted');
-            }, 1000);
-          });
+      build: {
+        files: publicFiles,
+        tasks: ['concat', 'uglify', 'less'],
+        options: {
+            livereload: true
         }
       }
     },
-    watch: {
-      'lint': {
-        files: ['<%= jshint.files %>', 'public/js/**/*.js', 'public/less/**/*.less'],
-        tasks: ['jshint', 'mochaTest:unit', 'concat', 'uglify', 'less'],
+    supervisor: {
+      target: {
+        script: "mammoth.js"
       },
-      'ut': {
-        files: '<%= jshint.files %>',
-        tasks: ['shell:ut']
-      }
+      options: {
+        debug: true,
+        watch: monitoredFiles,
+      },
     },
     concurrent: {
-      tasks: ['nodemon', 'watch'],
+      tasks: ['supervisor' , /*'watch:ut',*/ 'watch:build'],
       options: {
           logConcurrentOutput: true
-      }
-    },
-    shell: {
-      ut: {
-        command: 'grunt mochaTest:unit',
-        options: {
-          stderr: false
-        }
-      },
-      'ut-debug': {
-        command: 'mocha --require should --debug --debug-brk test/unit/**/*.js',
-        options: {
-          stderr: false
-        }
       }
     }
   });
@@ -153,15 +125,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-supervisor');
   grunt.loadNpmTasks('grunt-concurrent');
-  grunt.loadNpmTasks('grunt-shell');
 
-  // run typing "grunt ut"
-  grunt.registerTask('ut', ['watch:ut']);
-
-  // run typing "grunt ut-debug"
-  grunt.registerTask('ut-debug', ['shell:ut-debug']);
+  grunt.registerTask('jshint', ['jshint']);
 
   // run typing "grunt test"
   grunt.registerTask('test', ['jshint', 'mochaTest:all']);
@@ -173,9 +140,8 @@ module.exports = function (grunt) {
   grunt.registerTask('integration-test', ['jshint', 'mochaTest:integration']);
 
   // run typing "grunt build"
-  grunt.registerTask('build', [/*'jshint', 'mochaTest:all',*/ 'concat', 'uglify', 'less']);
+  grunt.registerTask('build', ['concat', 'uglify', 'less']);
 
   // run typing "grunt start"
   grunt.registerTask('start', ['concurrent']);
-
 };
