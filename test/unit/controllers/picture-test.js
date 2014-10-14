a436643cf512c06e3f4ea69d29a0e3b45e12ec40
Daniel Garcia Aubert <daniel.garcia@taptapnetworks.com>
2014-10-14 19:02:28 +0200

    Added some test

--- test/unit/controllers/picture-test.js
+++ test/unit/controllers/picture-test.js
@@ -7,19 +7,20 @@ describe('controllers/picture', function () {
   var PictureController = require('../../../lib/controllers/picture');
   var pictureController;
   var res = {
-    render: function () {},
+    set: function () {},
     redirect: function () {},
     send: function () {}
   };
   var req = {
-    body: {
-      author: 'irrelevantAuthor',
-      created: 'irrelevantCreated',
-      picture: 'irrelevantPicture',
-      mail: 'irrelevantMail@mail.com',
-    },
     params: {
-      slug: 'irrelevantSlug'
+      slug: ['irrelevantSlug'],
+      pictureName: ['irrelevantPictureName']
+    },
+    files: {
+      picture: {
+        path: 'irrelevantPath',
+        name: 'irrelevantName'
+      }
     }
   };
   var next = function () {};
@@ -31,13 +32,13 @@ describe('controllers/picture', function () {
 
   var sandbox;
   var pictureServiceStub;
-  var picture = req.body;
+  var picture = 0; // bytes
   var article = { slug: 'irrelevantSlug' };
 
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
 
-    res.render = sandbox.spy(res, 'render');
+    res.set = sandbox.spy(res, 'set');
     res.redirect = sandbox.spy(res, 'redirect');
     res.send = sandbox.spy(res, 'send');
     next = sandbox.spy(next);
@@ -50,60 +51,61 @@ describe('controllers/picture', function () {
     sandbox.restore();
   });
 
-  describe('.create(req, res)', function () {
+  describe('.retrieve(req, res, next)', function () {
 
-    it('should delegate to the service the picture\'s creation', function () {
-      pictureServiceStub.create.callsArgWith(3, null, picture);
+    it('should delegate to picture service', function () {
+      pictureServiceStub.retrieve.callsArgWith(1, null, picture);
+
+      pictureController.retrieve(req, res, next);
+
+      pictureServiceStub.retrieve.called.should.be.true;
+    });
+
+    it('should response with content-type "image/png"', function () {
+      pictureServiceStub.retrieve.callsArgWith(1, null, picture);
+
+      pictureController.retrieve(req, res, next);
+
+      res.set.calledWithExactly('Content-Type', 'image/png').should.be.true;
+      res.send.called.should.be.true;
+    });
+
+    it('when something went wrong it should should response with 500', function () {
+      var err = new Error();
+
+      pictureServiceStub.retrieve.callsArgWith(1, err, null);
+
+      pictureController.retrieve(req, res, next);
+
+      next.calledWith(err).should.be.true;
+    });
+
+  });
+
+  describe('.create(req, res, next)', function () {
+
+    it('should use the "create" method of picture service', function () {
+      pictureServiceStub.create.callsArgWith(3, null);
 
       pictureController.create(req, res, next);
 
       pictureServiceStub.create.called.should.be.true;
     });
 
-    it('should response with 200', function () {
-      pictureServiceStub.create.callsArgWith(3, null, picture);
+    it('should redirect "blog/admin/articles" view', function () {
+      pictureServiceStub.create.callsArgWith(3, null);
 
       pictureController.create(req, res, next);
 
-      res.send.calledWith(200).should.be.true;
-    });
-
-    it('when something went wrong it should should response with 500', function () {
-      var err = new Error();
-
-      pictureServiceStub.create.callsArgWith(3, err, null);
-
-      pictureController.create(req, res, next);
-
-      res.send.calledWith(500).should.be.true;
-    });
-
-  });
-
-  describe('.list(req, res, next)', function () {
-
-    it('should use the "list" method of picture service', function () {
-      pictureServiceStub.list.callsArgWith(1, null, [picture]);
-
-      pictureController.list(req, res, next);
-
-      pictureServiceStub.list.called.should.be.true;
-    });
-
-    it('should render "blog/admin/pictures" view', function () {
-      pictureServiceStub.list.callsArgWith(1, null, [picture]);
-
-      pictureController.list(req, res, next);
-
-      res.render.calledWith('blog/admin/pictures').should.be.true;
+      res.redirect.calledWith('/blog/admin/articles/' + req.params.slug[0]).should.be.true;
     });
 
     it('when something went wrong it should manage the incoming error', function () {
       var err = new Error();
 
-      pictureServiceStub.list.callsArgWith(1, err, null);
+      pictureServiceStub.create.callsArgWith(3, err);
 
-      pictureController.list(req, res, next);
+      pictureController.create(req, res, next);
 
       next.calledWith(err).should.be.true;
     });
@@ -113,29 +115,29 @@ describe('controllers/picture', function () {
   describe('.remove(req, res, next)', function () {
 
     it('should delegate to picture\'s service', function () {
-      pictureServiceStub.remove.callsArgWith(2, null, picture);
+      pictureServiceStub.remove.callsArgWith(1, null);
 
       pictureController.remove(req, res, next);
 
       pictureServiceStub.remove.called.should.be.true;
     });
 
-    it('should redirect to removed picture', function () {
-      pictureServiceStub.remove.callsArgWith(2, null, article);
+    it('should redirect to article of the picture', function () {
+      pictureServiceStub.remove.callsArgWith(1, null);
 
       pictureController.remove(req, res, next);
 
-      res.redirect.calledWith('/blog/admin/articles/' + req.params.slug + '/pictures/').should.be.true;
+      res.redirect.calledWith('/blog/admin/articles/' + req.params.slug).should.be.true;
     });
 
     it('when something went wrong it should manage the incoming error', function () {
       var err = new Error();
 
-      pictureServiceStub.remove.callsArgWith(2, err, null);
+      pictureServiceStub.remove.callsArgWith(1, err);
 
       pictureController.remove(req, res, next);
 
-      next.called.should.equal(true);
+      next.calledWith(err).should.equal(true);
     });
 
   });
